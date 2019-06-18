888623f419631b742f3f93656ebf265dcffdcb7f
Jason Shockness <jshock12@gmail.com>
2019-06-18 09:23:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ function theBeatlesPlay(musicians, intstruments) {
   return array
 }
 
+var facts = "He was the last Beatle to learn to drive",
+        "He was never a vegetarian",
+        "He was a choir boy and boy scout",
+        "He hated the sound of his own voice"
+
 function johnLennonFacts(facts) {
   var shoutedFacts
 }
