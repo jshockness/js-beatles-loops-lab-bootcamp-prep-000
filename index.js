87d5f853e2b0feef6e8c434c198ec3d405b18e00
Jason Shockness <jshock12@gmail.com>
2019-06-18 09:10:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ function theBeatlesPlay(musicians, intstruments) {
   for(let i = 0, l = musicians.length; i < l; i++) {
     array.push (`${musicians[i]} plays ${instrument[i]}`)
   }
+  return array
 }
