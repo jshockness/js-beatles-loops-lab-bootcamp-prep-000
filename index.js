b58248d3f3f324c6390096c658292fdfbbaee2ff
Jason Shockness <jshock12@gmail.com>
2019-06-18 09:19:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Star
 var intstruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
 
 function theBeatlesPlay(musicians, intstruments) {
-  const array = []
+  var array = []
   for(let i = 0, l = musicians.length; i < l; i++) {
     array.push (`${musicians[i]} plays ${intstruments[i]}`)
   }
